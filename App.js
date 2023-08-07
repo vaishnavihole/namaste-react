a5350c924e100c8b8738d83c0c9dd26298dfce55
vaishnavihole <vaishnavihole1@gmail.com>
2023-08-07 22:20:03 +0530

    add redme.md file

--- App.js
+++ App.js
@@ -5,19 +5,30 @@
       <h2>I am h2 tag</h2>
     </div>
 
+     <div id="parent">
+    <div id='child'>
+      <h1>I am h1 tag</h1>
+      <h2>I am h2 tag</h2>
+    </div>
+
 </div> */}
 
-const parent = React.createElement(
-    "div", 
-    { id: "parent" }, 
-    React.createElement("div", {id: "child"}, [
-    React.createElement("h1", {}, "I am  an h1 tag"), 
-    React.createElement("h2", {}, "I am  an h2 tag")
-])
+const parent = React.createElement("div",{ id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am  an h1 tag"),
+        React.createElement("h2", {}, "I am  an h2 tag")
+    ]),
+    
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am  an h1 tag"),
+        React.createElement("h2", {}, "I am  an h2 tag")
+    ]),
 );
 
-    console.log(parent); //object
+// JSX
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent); //object
 
-    root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
