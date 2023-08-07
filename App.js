507e1a071ff95f2372c200184c61cb1e41c891eb
vaishnavihole <vaishnavihole1@gmail.com>
2023-08-07 10:36:48 +0530

    add css

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-const heading =  React.createElement("h1", {},  "Hello World from react");
+const heading =  React.createElement(
+"h1", 
+{id: "heading", xyz: "abc"},  
+"Hello World from react"
+);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-
-        root.render(heading);
+    root.render(heading);
