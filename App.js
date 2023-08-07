14a4710948c7bee4fa282c028d5abdf8fb902ef8
vaishnavihole <vaishnavihole1@gmail.com>
2023-08-07 10:28:44 +0530

    add script in app.js file

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading =  React.createElement("h1", {},  "Hello World from react");
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
