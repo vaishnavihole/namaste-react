f3cdcb82e879209b6f9989a092d9037586720044
vaishnavihole <vaishnavihole1@gmail.com>
2023-08-09 11:40:33 +0530

    create an element

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div",{ id: "parent" },
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "This is Namste React"),
-        React.createElement("h2", {}, "I am  an h2 tag")
-    ]),
-    
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am  an h1 tag"),
-        React.createElement("h2", {}, "I am  an h2 tag")
-    ]),
+
+//  React.createElement
+
+const heading = React.createElement("h1", 
+{id: "heading"},
+"Namaste React"
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-// JSX
-
-
-console.log(parent); //object
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(heading);
